7bd8508e95c040bf232be1f852aea29ccd04bb94
João Carlos <joao.carlos_25@hotmail.com>
2021-10-19 10:50:28 -0300

    Implementação Lombok

--- src/main/java/dev/scjoao/ProjetoSpringProdutos/Entities/Category.java
+++ src/main/java/dev/scjoao/ProjetoSpringProdutos/Entities/Category.java
@@ -3,7 +3,6 @@ package dev.scjoao.ProjetoSpringProdutos.Entities;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -13,7 +12,16 @@ import javax.persistence.OneToMany;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 @Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Category implements Serializable {
 	
 	private static final long serialVersionUID = 1L;
@@ -27,50 +35,9 @@ public class Category implements Serializable {
 	@OneToMany(mappedBy = "category")
 	private List<Product> products = new ArrayList<>(); 
 	
-	public Category() {
-		
-	}
-	
 	public Category(Long id, String name) {
 		super();
 		this.id = id;
 		this.name = name;
-	}
-
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-	
-	public List<Product> getProducts() {
-		return products;
-	}
-
-	@Override
-	public int hashCode() {
-		return Objects.hash(id);
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Category other = (Category) obj;
-		return Objects.equals(id, other.id);
 	}
 }
--- src/main/java/dev/scjoao/ProjetoSpringProdutos/Entities/Product.java
+++ src/main/java/dev/scjoao/ProjetoSpringProdutos/Entities/Product.java
@@ -1,7 +1,6 @@
 package dev.scjoao.ProjetoSpringProdutos.Entities;
 
 import java.io.Serializable;
-import java.util.Objects;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -10,7 +9,16 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 @Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Product implements Serializable {
 	
 	private static final long serialVersionUID = 1L;
@@ -27,64 +35,4 @@ public class Product implements Serializable {
 	@JoinColumn(name = "id_category")
 	private Category category;
 	
-	public Product() {
-		
-	}
-
-	public Product(Long id, String name, Double price, Category category) {
-		super();
-		this.id = id;
-		this.name = name;
-		this.price = price;
-		this.category = category;
-	}
-
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public Double getPrice() {
-		return price;
-	}
-
-	public void setPrice(Double price) {
-		this.price = price;
-	}
-
-	public Category getCategory() {
-		return category;
-	}
-
-	public void setCategory(Category category) {
-		this.category = category;
-	}
-
-	@Override
-	public int hashCode() {
-		return Objects.hash(id);
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Product other = (Product) obj;
-		return Objects.equals(id, other.id);
-	}
 }
